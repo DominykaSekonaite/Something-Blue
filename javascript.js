94d510539a1642689cb549450ecfa1e87ac92bf8
NikiDudas <89193237+NikiDudas@users.noreply.github.com>
2024-06-09 19:14:18 +0100

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,73 +1,42 @@
-// // Open the Modal
-// function openModal() {
-//     document.getElementById("myModal").style.display = "block";
-//   }
-  
-//   // Close the Modal
-//   function closeModal() {
-//     document.getElementById("myModal").style.display = "none";
-//   }
-  
-//   var slideIndex = 1;
-//   showSlides(slideIndex);
-  
-//   // Next/previous controls
-//   function plusSlides(n) {
-//     showSlides(slideIndex += n);
-//   }
-  
-//   function showSlides(n) {
-//     var i;
-//     var slides = document.getElementsByClassName("mySlides");
-//     var dots = document.getElementsByClassName("demo");
-//     var captionText = document.getElementById("caption");
-//     if (n > slides.length) {slideIndex = 1}
-//     if (n < 1) {slideIndex = slides.length}
-//     for (i = 0; i < slides.length; i++) {
-//       slides[i].style.display = "none";
-//     }
-//     for (i = 0; i < dots.length; i++) {
-//       dots[i].className = dots[i].className.replace("active2", "");
-//     }
-//     slides[slideIndex-1].style.display = "block";
-//     dots[slideIndex-1].className += "active2";
-//     captionText.innerHTML = dots[slideIndex-1].alt;
-//   }
-
 function openModal() {
   document.getElementById("myModal").style.display = "block";
-}
-
-function closeModal() {
+ }
+ 
+ 
+ function closeModal() {
   document.getElementById("myModal").style.display = "none";
-}
-
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
+ }
+ 
+ 
+ var slideIndex = 1;
+ showSlides(slideIndex);
+ 
+ 
+ function plusSlides(n) {
   showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
+ }
+ 
+ 
+ function currentSlide(n) {
   showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
+ }
+ 
+ 
+ function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
+  //var dots = document.getElementsByClassName("demo");
   var captionText = document.getElementById("caption");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace("active2", "");
-  }
+  // for (i = 0; i < dots.length; i++) {
+  //     dots[i].className = dots[i].className.replace("active2", "");
+  // }
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += "active2";
-  captionText.innerHTML = dots[slideIndex-1].alt;
-}
-
+  // dots[slideIndex-1].className += "active2";
+  //captionText.innerHTML = "POXUI";//dots[slideIndex-1].alt;
+ }
+ 
